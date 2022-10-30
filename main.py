1e59787201ef883f491ac3c3c64adb5b87546d7e
curtisrencher7 <116689582+curtisrencher7@users.noreply.github.com>
2022-10-30 04:42:30 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+def on_wifi_connected():
+    pass
+kittenwifi.on_wifi_connected(on_wifi_connected)
+
+def on_udp_ondata(udpData):
+    pass
+kittenwifi.udp_ondata(on_udp_ondata)
+
 def on_sound_loud():
     pass
 input.on_sound(DetectedSound.LOUD, on_sound_loud)
 
-def on_every_interval():
+def on_mqtt_topic_data(topic, data):
     pass
+kittenwifi.on_mqtt_topic_data(on_mqtt_topic_data)
+
+def on_every_interval():
+    list2: List[number] = []
+    for value in list2:
+        pass
 loops.every_interval(500, on_every_interval)
 
 def on_forever():
--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
-input.onSound(DetectedSound.Loud, function () {
-	
+kittenwifi.on_wifi_connected(function on_wifi_connected() {
+    
 })
-loops.everyInterval(500, function () {
-	
+kittenwifi.udp_ondata(function on_udp_ondata(udpData: string) {
+    
 })
-basic.forever(function () {
-	
+input.onSound(DetectedSound.Loud, function on_sound_loud() {
+    
+})
+kittenwifi.on_mqtt_topic_data(function on_mqtt_topic_data(topic: string, data: string) {
+    
+})
+loops.everyInterval(500, function on_every_interval() {
+    let list2 : number[] = []
+    for (let value of list2) {
+        
+    }
+})
+basic.forever(function on_forever() {
+    
 })
